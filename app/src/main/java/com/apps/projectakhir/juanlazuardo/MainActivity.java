1b810f1752761e4cb72c0321fa543a6ad3dc054f
Only Wan <lazuardojuan@gmail.com>
2020-06-20 19:50:52 +0700

    First Upload Project

---
+++ app/src/main/java/com/apps/projectakhir/juanlazuardo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.apps.projectakhir.juanlazuardo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+NIM     : 10117160
+Nama    : Juan Lazuardo
+Kelas   : IF-4
+
+Update :
+    19 Juli 2020    : - Membuat Asset Splash Screen dan MainActivity
+    20 Juli 2020    : - Membuat Tampilan Splash Screen dan MainActivity
+                      - Upload Github --> First Upload Project
+
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/apps/projectakhir/juanlazuardo/Splash.java
@@ -0,0 +1,23 @@
+package com.apps.projectakhir.juanlazuardo;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
